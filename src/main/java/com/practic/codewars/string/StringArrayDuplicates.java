8508f40655bf540a8a0e7ce4b4532bf5ba6636e1
Taras Kuzub <taras.kuzub.ua@gmail.com>
2020-09-24 02:57:16 +0300

    solve task "String array duplicates" CodeWars

---
+++ src/main/java/com/practic/codewars/string/StringArrayDuplicates.java
@@ -0,0 +1,36 @@
+package com.practic.codewars.string;
+/*In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate
+letters from each string in the array.
+
+For example:
+
+dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+dup(["kelless","keenness"]) = ["keles","kenes"].
+
+Strings will be lowercase only, no spaces. See test cases for more examples.*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringArrayDuplicates {
+    public String[] dup(String[] arr){
+        List<String> list = new ArrayList<>();
+        List<String> resultList = new ArrayList<>();
+        for (String string : arr) {
+            removeDuplicates(list, resultList, string);
+        }
+       return resultList.toArray(String[]::new);
+    }
+
+    private void removeDuplicates(List<String> list, List<String> resultList, String string) {
+        list.clear();
+        for (int index = 0; index < string.length() - 1; index++) {
+            if (string.charAt(index) != string.charAt(index + 1)) {
+                list.add(string.substring(index, index + 1));
+            }
+        }
+        String resultString = String.join("", list);
+        resultList.add(resultString + string.substring(string.length() - 1));
+    }
+}
