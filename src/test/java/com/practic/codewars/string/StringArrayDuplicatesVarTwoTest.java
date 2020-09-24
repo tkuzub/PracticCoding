068e9ecd09b1a6d660c3046d01ba6c33c1136766
Taras Kuzub <taras.kuzub.ua@gmail.com>
2020-09-24 03:16:48 +0300

    solve task "String array duplicates" CodeWars looping solution

---
+++ src/test/java/com/practic/codewars/string/StringArrayDuplicatesVarTwoTest.java
@@ -0,0 +1,22 @@
+package com.practic.codewars.string;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class StringArrayDuplicatesVarTwoTest {
+    private StringArrayDuplicatesVarTwo duplicates;
+    
+    @Before
+    public void setUp() {
+        duplicates = new StringArrayDuplicatesVarTwo();
+    }
+
+    @Test
+    public void basicTests() {
+        assertArrayEquals(new String[]{"codewars","picaniny","hubububo"},duplicates.dup(new String[]{"ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"}));
+        assertArrayEquals(new String[]{"abracadabra","alote","asese"},duplicates.dup(new String[]{"abracadabra","allottee","assessee"}));
+        assertArrayEquals(new String[]{"keles","kenes"},duplicates.dup(new String[]{"kelless","keenness"}));
+    }
+}
