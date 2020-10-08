d705eb00bc640cf2a74e286eb8e2961e7c5e7866
Taras Kuzub <taras.kuzub.ua@gmail.com>
2020-10-08 13:54:01 +0300

    solve task "Fun with lists: indexOf" CodeWars 7kuy

---
+++ src/main/java/com/practic/codewars/list/MyList2.java
@@ -0,0 +1,36 @@
+package com.practic.codewars.list;
+
+import java.util.List;
+
+/**
+ * For example: Given the list: 1 -> 2 -> 3 -> 3, and the value 3, indexOf / index_of should return 2.
+ * <p>
+ * The linked list is defined as follows:
+ */
+
+public class MyList2 {
+    private Node head;
+    private int size;
+
+    static class Node {
+        public Node next;
+        public Object data;
+
+        Node(Object data) {
+            this.data = data;
+        }
+    }
+
+    public int indexOf(Node head, Object value) {
+        Node curr = head;
+        int count = 0;
+        while (curr != null) {
+            if (curr.data.equals(value)) {
+                return count;
+            }
+            ++count;
+            curr = curr.next;
+        }
+        return -1;
+    }
+}
