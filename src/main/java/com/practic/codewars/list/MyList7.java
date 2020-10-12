6eb27d2e60ab163b81b9c08b9d941b61e5c2a972
Taras Kuzub <taras.kuzub.ua@gmail.com>
2020-10-12 18:37:25 +0300

    solve task "Fun with lists: map CodeWars 6kuy

---
+++ src/main/java/com/practic/codewars/list/MyList7.java
@@ -0,0 +1,42 @@
+package com.practic.codewars.list;
+
+import java.util.function.Function;
+
+/**
+ * Implement the method map, which accepts a linked list (head) and a mapping function, and returns a new linked list
+ * (head) where every element is the result of applying the given mapping method to each element of the original list.
+ *
+ * For example: Given the list: 1 -> 2 -> 3, and the mapping function x => x * 2, map should return 2 -> 4 -> 6
+ * @param <T>
+ */
+
+public class MyList7<T> {
+    private Node<T> head;
+
+    static class Node<T> {
+        public T data;
+        public Node<T> next;
+
+        Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        Node(T data) {
+            this(data, null);
+        }
+    }
+
+    public static <T, R> Node<R> map(Node<T> head, Function<T, R> f) {
+        Node<T> curr = head;
+        Node<R> newNode = new Node<>(null);
+        Node<R> temp = newNode;
+
+        while (curr != null) {
+            temp.next = new Node<>(f.apply(curr.data));
+            temp = temp.next;
+            curr = curr.next;
+        }
+        return newNode.next;
+    }
+}
