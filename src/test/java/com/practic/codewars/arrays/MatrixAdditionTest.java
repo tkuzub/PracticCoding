40ce0d814d2f61bc55fce45a93f819dbccfadea0
Taras Kuzub <taras.kuzub.ua@gmail.com>
2020-11-01 00:04:13 +0200

    solve task "Matrix Addition" CodeWars 6kuy

---
+++ src/test/java/com/practic/codewars/arrays/MatrixAdditionTest.java
@@ -0,0 +1,37 @@
+package com.practic.codewars.arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MatrixAdditionTest {
+    private MatrixAddition addition;
+
+    @Before
+    public void setUp() {
+        addition = new MatrixAddition();
+    }
+
+    @Test
+    public void sampleTest() {
+        int[][] expected = new int[][] { { 3, 4, 4 }, { 6, 4, 4 }, { 2, 2, 4 } };
+
+        assertArrayEquals(expected,
+                addition.matrixAddition(
+                        new int[][] { { 1, 2, 3 }, { 3, 2, 1 }, { 1, 1, 1 } },
+                        new int[][] { { 2, 2, 1 }, { 3, 2, 3 }, { 1, 1, 3 } }));
+
+        expected = new int[][] { { 3 } };
+        assertArrayEquals(expected,
+                addition.matrixAddition(
+                        new int[][] { { 1 } },
+                        new int[][] { { 2 } }));
+
+        expected = new int[][] { { 3, 5 }, { 3, 5 } };
+        assertArrayEquals(expected,
+                addition.matrixAddition(
+                        new int[][] { { 1, 2 }, { 1, 2 } },
+                        new int[][] { { 2, 3 }, { 2, 3 } }));
+    }
+}
