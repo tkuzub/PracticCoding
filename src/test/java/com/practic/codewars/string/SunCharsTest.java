f15967c751694715caa92361c910fe9b07dc6697
Taras Kuzub <taras.kuzub.ua@gmail.com>
2020-11-13 15:52:42 +0200

    solve task "Compare Strings by Sum of Chars" CodeWars 7kuy

---
+++ src/test/java/com/practic/codewars/string/SunCharsTest.java
@@ -0,0 +1,22 @@
+package com.practic.codewars.string;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SunCharsTest {
+    private SunChars sunChars;
+
+    @Before
+    public void setUp() {
+        sunChars = new SunChars();
+    }
+
+    @Test
+    public void BasicTests() {
+        assertTrue(sunChars.compare("AD", "BC"));
+        assertFalse(sunChars.compare("AD", "DD"));
+        assertTrue(sunChars.compare("gf", "FG"));
+    }
+}
