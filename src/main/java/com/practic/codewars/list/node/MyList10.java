c64c345d3f737365889d8fed61c782493251114a
Taras Kuzub <taras.kuzub.ua@gmail.com>
2020-11-17 16:20:37 +0200

    solve task "Linked Lists - Append" CodeWars 7kuy

---
+++ src/main/java/com/practic/codewars/list/node/MyList10.java
@@ -0,0 +1,57 @@
+/*Linked Lists - Append
+
+Write an Append() function which appends one linked list to another. The head of the resulting list should be returned.
+
+var listA = 1 -> 2 -> 3 -> null
+var listB = 4 -> 5 -> 6 -> null
+append(listA, listB) === 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+If both listA and listB are null/NULL/None/nil, return null/NULL/None/nil. If one list is null/NULL/None/nil and the
+other is not, simply return the non-null/NULL/None/nil list.*/
+
+package com.practic.codewars.list.node;
+
+public class MyList10 {
+
+    private Node head;
+    private int size;
+
+    static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public static Node append(Node listA, Node listB) {
+            if (listA == null) {
+                return listB;
+            }
+
+            if (listB == null) {
+                return listA;
+            }
+
+            Node curr = listA;
+            Node temp = listB;
+
+            while (curr != null) {
+                if (curr.next == null) {
+                    while (temp != null) {
+                        curr.next = new Node(temp.data);
+                        curr = curr.next;
+                        temp = temp.next;
+                    }
+                }
+                curr = curr.next;
+            }
+            return listA;
+        }
+    }
+}
