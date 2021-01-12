89183d7b9835d67c9912fec024aa488054971127
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-01-12 15:23:44 +0200

    solve task "Vowel Count" 7kyu CodeWars, variant of the problem with chars() and indexOF

---
+++ src/main/java/com/practic/indigo/string/VowelsVar2.java
@@ -0,0 +1,14 @@
+/*Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.*/
+
+package com.practic.indigo.string;
+
+public class VowelsVar2 {
+    public int getCount(String str) {
+        String vowels = "aoieu";
+        return (int)str.chars().filter(c -> vowels.indexOf(c) != -1).count();
+    }
+}
