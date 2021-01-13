ac7d5927afd8a03a5312db96b6f1130007f94501
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-01-13 16:52:40 +0200

    solve task "String ends with?" 7kyu CodeWars

---
+++ src/test/java/com/practic/indigo/string/StringEndsTest.java
@@ -0,0 +1,20 @@
+package com.practic.indigo.string;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringEndsTest {
+    private StringEnds stringEnds;
+
+    @Before
+    public void setUp() {
+        stringEnds = new StringEnds();
+    }
+
+    @Test
+    public void testSomething() {
+         assertEquals("expected", "actual");
+    }
+}
