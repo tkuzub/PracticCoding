98005e14ddc22e28ebe7d369935f77cb3af393dd
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-01-13 16:20:06 +0200

    solve task through the loop "Reversed Strings" 8kyu CodeWars

---
+++ src/main/java/com/practic/indigo/string/ReversedStringsVarTwo.java
@@ -0,0 +1,18 @@
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'*/
+
+package com.practic.indigo.string;
+
+public class ReversedStringsVarTwo {
+    public String solution(String str) {
+        char[] chars = str.toCharArray();
+
+        for (int index = 0; index < chars.length / 2; index++) {
+            char temp = chars[index];
+            chars[index] = chars[chars.length - index - 1];
+            chars[chars.length - index - 1] = temp;
+        }
+        return new String(chars);
+    }
+}
