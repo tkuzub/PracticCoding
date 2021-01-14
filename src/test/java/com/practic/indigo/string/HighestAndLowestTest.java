2e0797ed4562c3af9c93d19d4a71d5848da77035
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-01-14 15:53:28 +0200

    solve task "Highest and Lowest" 7kyu CodeWars

---
+++ src/test/java/com/practic/indigo/string/HighestAndLowestTest.java
@@ -0,0 +1,20 @@
+package com.practic.indigo.string;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HighestAndLowestTest {
+    private HighestAndLowest highestAndLowest;
+
+    @Before
+    public void setUp() {
+        highestAndLowest = new HighestAndLowest();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals("42 -9", highestAndLowest.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+    }
+}
