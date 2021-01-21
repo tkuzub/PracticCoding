7d1040babea12a24416278b82ea835d4171ed692
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-01-21 16:41:27 +0200

    solve task "Array plus array" 8kyu CodeWars

---
+++ src/main/java/com/practic/indigo/array/ArrayPlusArray.java
@@ -0,0 +1,15 @@
+
+/*I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
+I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.*/
+
+package com.practic.indigo.array;
+
+import java.util.Arrays;
+
+public class ArrayPlusArray {
+    public int arrayPlusArray(int[] arr1, int[] arr2) {
+        return Arrays.stream(arr1).sum() + Arrays.stream(arr2).sum();
+    }
+}
---
+++ src/test/java/com/practic/indigo/array/ArrayPlusArrayTest.java
@@ -0,0 +1,23 @@
+package com.practic.indigo.array;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayPlusArrayTest {
+    private ArrayPlusArray arraysSum;
+
+    @Before
+    public void setUp() {
+        arraysSum = new ArrayPlusArray();
+    }
+
+    @Test
+    public void sampleTests() {
+        assertEquals(21, arraysSum.arrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6}));
+        assertEquals(-21, arraysSum.arrayPlusArray(new int[]{-1,-2,-3}, new int[]{-4,-5,-6}));
+        assertEquals(15, arraysSum.arrayPlusArray(new int[]{0,0,0}, new int[]{4,5,6}));
+        assertEquals(2100, arraysSum.arrayPlusArray(new int[]{100,200,300}, new int[]{400,500,600}));
+    }
+}
