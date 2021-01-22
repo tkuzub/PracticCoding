249b9dfb809ad97723aaaefa54a4983dbb0d3d59
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-01-22 02:49:27 +0200

    solve the problem "Odd or Even?" 7kyu CodeWars

---
+++ src/test/java/com/practic/indigo/array/OddEvenTest.java
@@ -0,0 +1,20 @@
+package com.practic.indigo.array;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OddEvenTest {
+    private OddEven oddEven;
+
+    @Before
+    public void setUp() {
+        oddEven = new OddEven();
+    }
+
+    @Test
+    public void exampleTest() {
+        assertEquals("odd", oddEven.oddOrEven(new int[] {2, 5, 34, 6}));
+    }
+}
