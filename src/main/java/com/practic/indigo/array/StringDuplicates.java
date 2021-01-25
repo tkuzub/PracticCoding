125adcfc5f1b0c4697f0774172a81e5b5b2cec06
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-01-25 23:04:22 +0200

    solve the problem (refactoring) "String array duplicates" 6kyu CodeWars

--- src/main/java/com/practic/indigo/array/StringDuplicates.java
+++ src/main/java/com/practic/indigo/array/StringDuplicates.java
@@ -13,21 +13,22 @@ Good luck!*/
 
 package com.practic.indigo.array;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class StringDuplicates {
     public String[] dup(String[] arr){
-
         String[] result = new String[arr.length];
+
         int currentIndex = 0;
         StringBuilder string;
+
         for (String str : arr) {
             string = new StringBuilder();
             for (int i = 0; i < str.length() - 1; i++) {
-                if (!(str.substring(i, i + 1).equals(str.substring(i + 1, i + 2)))) {
+                if (!(str.charAt(i) == str.charAt(i + 1))) {
                     string.append(str.charAt(i));
                 }
+            }
+            if (str.charAt(str.length() - 1) != string.charAt(string.length() - 1)) {
+                string.append(str.charAt(str.length() - 1));
             }
             result[currentIndex++] = string.toString();
         }
