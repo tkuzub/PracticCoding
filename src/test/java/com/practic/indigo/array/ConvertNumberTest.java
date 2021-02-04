d5c36a1ba4fe1f994566e1964520f6834119f405
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-02-04 22:24:27 +0200

    solve the problem "Convert number to reversed array of digits" 8kyu CodeWars

---
+++ src/test/java/com/practic/indigo/array/ConvertNumberTest.java
@@ -0,0 +1,20 @@
+package com.practic.indigo.array;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConvertNumberTest {
+    private ConvertNumber convertNumber;
+
+    @Before
+    public void setUp() {
+        convertNumber = new ConvertNumber();
+    }
+
+    @Test
+    public void tests() {
+        assertArrayEquals(new int[] {1, 3, 2, 5, 3}, convertNumber.digitize(35231));
+    }
+}
