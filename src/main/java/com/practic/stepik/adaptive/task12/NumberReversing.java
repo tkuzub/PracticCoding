c79207edc2c4b7481e340d8723d8ca44b67f9fa4
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-02-06 11:00:20 +0200

    solve the problem "Write a program that reads a three digit number, calculates the new number by reversing its digits, and outputs a new number." from adaptive

---
+++ src/main/java/com/practic/stepik/adaptive/task12/NumberReversing.java
@@ -0,0 +1,40 @@
+package com.practic.stepik.adaptive.task12;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Write a program that reads a three digit number, calculates the new number by reversing its digits, and outputs
+ * a new number.
+ *
+ * Sample Input:
+ *
+ * 682
+ * Sample Output:
+ *
+ * 286
+ */
+public class NumberReversing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String data = scanner.nextLine();
+
+        List<Integer> list = IntStream.range(0, data.length()).
+                map(x -> Integer.parseInt(data.substring(x, x + 1))).boxed().
+                collect(Collectors.toList());
+        Collections.reverse(list);
+
+        String result = list.stream().map(String::valueOf).collect(Collectors.joining());
+        System.out.println(result);
+    }
+}
+
+
+/*System.out.println(new StringBuilder((new java.util.Scanner(System.in)).next()).reverse().toString());*/
+
+/*Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(n / 100 + ((n / 10) % 10) * 10 + (n % 10) * 100);*/
