f5ea9c0c6752d7163e9cccfc0880ab155c47e8b3
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-02-16 16:17:09 +0200

    Solved problem "Square(n) Sum" Kata 7kue From CodeWars

---
+++ src/test/java/com/practic/indigo/array/SquareSumTest.java
@@ -0,0 +1,22 @@
+package com.practic.indigo.array;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SquareSumTest {
+    private SquareSum squareSum;
+
+    @Before
+    public void setUp() {
+        squareSum = new SquareSum();
+    }
+
+    @Test
+    public void testBasic() {
+        assertEquals(9, squareSum.squareSum(new int[] {1,2,2}));
+        assertEquals(5, squareSum.squareSum(new int[] {1,2}));
+        assertEquals(50, squareSum.squareSum(new int[] {5,-3,4}));
+    }
+}
