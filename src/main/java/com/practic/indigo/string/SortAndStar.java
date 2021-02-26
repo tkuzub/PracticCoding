63edf456a38be2388516cfcdb91092b25e99c2a2
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-02-26 20:48:21 +0200

    I have solved task "Sort and Star" 8kuy from codeWars

---
+++ src/main/java/com/practic/indigo/string/SortAndStar.java
@@ -0,0 +1,19 @@
+package com.practic.indigo.string;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII
+ * values of the chars) and then return the first value.
+ *
+ * The returned value must be a string, and have "***" between each of its letters.
+ *
+ * You should not remove or add elements from/to the array.
+ */
+public class SortAndStar {
+    public String twoSort(String[] s) {
+        Arrays.sort(s);
+        return String.join("***", s[0].split(""));
+    }
+}
