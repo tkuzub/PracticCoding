396366816c15d20396ad8c3c27ca655eef3c7c9c
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-03-02 18:56:23 +0200

    I have solved task "Alphabet war" 7kuy from codeWars

---
+++ src/test/java/com/practic/indigo/string/AlphabetWarTest.java
@@ -0,0 +1,23 @@
+package com.practic.indigo.string;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AlphabetWarTest {
+    private AlphabetWar alphabetWar;
+
+    @Before
+    public void setUp() {
+        alphabetWar = new AlphabetWar();
+    }
+
+    @Test
+    public void BasicTest() {
+        assertEquals("Right side wins!", alphabetWar.alphabetWar("z"));
+        assertEquals("Let's fight again!", alphabetWar.alphabetWar("zdqmwpbs"));
+        assertEquals("Right side wins!", alphabetWar.alphabetWar("zzzzs"));
+        assertEquals("Left side wins!", alphabetWar.alphabetWar("wwwwwwz"));
+    }
+}
