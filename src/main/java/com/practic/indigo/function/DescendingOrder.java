891995f1e6d05d66885cadd39c7b705a565412b9
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-03-09 16:05:53 +0200

    I have solved task "Descending Order" 7kuy from codeWars

---
+++ src/main/java/com/practic/indigo/function/DescendingOrder.java
@@ -0,0 +1,24 @@
+package com.practic.indigo.function;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+/**
+ * Your task is to make a function that can take any non-negative integer as an argument and return it with its
+ * digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+ *
+ * Examples:
+ * Input: 42145 Output: 54421
+ *
+ * Input: 145263 Output: 654321
+ *
+ * Input: 123456789 Output: 987654321
+ */
+public class DescendingOrder {
+    public int sortDesc(final int num) {
+        return Integer.parseInt(Arrays.stream(String.valueOf(num).split("")).
+                sorted(Comparator.reverseOrder()).
+                collect(Collectors.joining()));
+    }
+}
